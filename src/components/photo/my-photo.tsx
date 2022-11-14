dc867c3fbde5fee95c13259a253d6ec111a4df4f
Jiri Fiala <jirik.fiala@post.cz>
2022-11-14 11:02:16 +0100

    image resize

--- src/components/photo/my-photo.tsx
+++ src/components/photo/my-photo.tsx
@@ -1,3 +1,4 @@
+import me from '../../../public/images/me-scaled.jpg'
 import myPhoto3 from "../../../public/images/me-4.jpg";
 import Image from "next/image";
 import { Flex } from "@chakra-ui/react";
@@ -5,7 +6,7 @@ import { Flex } from "@chakra-ui/react";
 const MyPhoto: React.FC = () => {
   return (
     <Flex alignItems="center" direction="column" >
-      <Image src={myPhoto3} alt="Jiri Fiala" priority />
+      <Image src={me} alt="Jiri Fiala" priority />
     </Flex>
   );
 };
